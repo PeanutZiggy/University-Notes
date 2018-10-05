d43854baf3ea3d87945788d19f8dccee59f351d3
Kaleem <KaleemPeeroo@gmail.com>
2018-10-05 12:31:58 +0100

    Added php file

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>University Notes</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<link rel="stylesheet" href="css/indexStyle.css">
+	</head>
+	<body>
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+		<div class="container">
+			<h1>University Notes</h1>
+
+			<div class="list-group" id="module_list">
+				<a href="dsa/index.html" class="list-group-item list-group-item-action">IN2002 Data Structures and Algorithms</a>
+				<a href="ooad/index.html" class="list-group-item list-group-item-action">IN2013 Object Oriented Analysis and Design</a>
+				<a href="pdit/index.html" class="list-group-item list-group-item-action">IN2015 Professional Development in IT</a>
+				<a href="cpp/index.html" class="list-group-item list-group-item-action">IN2029 Programming in C++</a>
+			</div>
+		</div>
+
+	</body>
+</html>
